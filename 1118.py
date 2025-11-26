fcb1dd492b40062fcb29b5acee46b1440177c171
CotrimGabriel <gabrielcotrim9276@gmail.com>
2025-11-26 20:08:39 -0300

    1118

--- 1118.py
+++ 1118.py
@@ -1,22 +1,35 @@
-controle=1
+repeticao = 1
+controle = 1
+controle_ava1 = 1
+controle_ava2 = 1
 
-while(controle!=2):
-    controle=2
-    primiera_ava = int(input('insira a avalicao numero 1: '))
-    segunda_ava = int(input('insira a avalicao numero 2: '))
+while controle != 2:
+    controle_ava1 = 1
+    controle_ava2 = 1
 
-    if primiera_ava>10 or primiera_ava<0:
-        controle+=1
-    elif segunda_ava>10 or segunda_ava<0:
-        controle+=1
-        
-    if controle>2:
-        print('nota invalida')
+    while controle_ava1 != 0:
+        primiera_ava = float(input())
+        if primiera_ava < 0 or primiera_ava > 10:
+            print("nota invalida")
+        else:
+            controle_ava1 = 0
+
+    while controle_ava2 != 0:
+        segunda_ava = float(input())
+        if segunda_ava < 0 or segunda_ava > 10:
+            print("nota invalida")
+        else:
+            controle_ava2 = 0
+
+    media = (primiera_ava + segunda_ava) / 2
+    print(f"media = {media:.2f}")
+
+    repeticao = 0
+    while repeticao != 1 and repeticao != 2:
+        print("novo calculo (1-sim 2-nao)")
+        repeticao = int(input())
+
+    if repeticao == 1:
+        controle = 1
     else:
-        media = (primiera_ava+segunda_ava)/2
-        print(f'{media:.2f}')
-        repeticao=int(input('novo calculo? (1-sim 2-nao)'))
-        if repeticao==1:
-            controle+=1
-
-    
+        controle = 2
